a05f81513aafadb24828914f8f2161c66a4f44aa
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-17 18:10:08 -0300

    chore(vr-header): update module federation plugin

--- vr-header/vite.config.ts
+++ vr-header/vite.config.ts
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { federation } from '@module-federation/vite'
-
-import { dependencies } from './package.json'
+import ModuleFederationPlugin from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
+  build: {
+		target: 'chrome89',
+	},
   plugins: [
-    federation({
-      name: 'vr-header',
-      remotes: {},
-      exposes: {},
-      shared: {
-        react: {
-          requiredVersion: dependencies.react,
-          singleton: true
-        }
-      }
+    ModuleFederationPlugin({
+      name: "vr-header",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/components/Header"
+      },
+      shared: ["react", "react-dom"],
     }),
     react()
   ],
