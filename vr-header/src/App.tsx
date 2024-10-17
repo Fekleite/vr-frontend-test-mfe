091d4ea1b4255d296d8527c4b2a423497aff2947
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-17 14:32:17 -0300

    feat(vr-header): create header component

--- vr-header/src/App.tsx
+++ vr-header/src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from "./components/Header";
+
+import "./index.css";
+
 export function App() {
   return (
     <div>
-      <span>header</span>
+      <Header />
     </div>
   )
 }
---
+++ vr-header/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logo from '../../assets/logo-white.png'
+import { BagIcon } from '../BagIcon'
+
+export function Header() {
+  return (
+    <header className='w-full bg-brand py-4'>
+      <div className='w-full max-w-[1440px] px-12 mx-auto flex justify-between items-center'>
+        <img src={logo} alt="VR" className='w-8 h-8' />
+
+        <div className='flex'>
+          <button type='button' className='h-8 px-4 bg-neutral-900 rounded-full hover:bg-neutral-700 duration-200'>
+            <BagIcon />
+
+            <span className='sr-only'>Sacola</span>
+          </button>
+        </div>
+      </div>
+    </header>
+  )
+}
