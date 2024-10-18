2b5b457622e7beb734ad90e7c8303d50c2ed98ab
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-17 23:24:09 -0300

    chore(vr-cards): add tailwind config

---
+++ vr-cards/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'brand': '#02D72F'
+      }
+    },
+  },
+  plugins: [],
+}
+
