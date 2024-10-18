40553f3ef7e9fcaafbc85a4c8d689ba510337d3a
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-18 09:50:46 -0300

    feat(vr-cards): list real products of api

--- vr-cards/src/components/Cards/index.tsx
+++ vr-cards/src/components/Cards/index.tsx
@@ -1,19 +1,41 @@
+import { useQuery } from "@tanstack/react-query"
+
+import { getProducts } from "../../api/get-products"
+
+import { Skeleton } from "../Skeleton"
+
+
 export default function Cards() {
+  const { data, isLoading } = useQuery({
+    queryFn: () => getProducts(),
+    queryKey: ['products']
+  })
+
+  function formatPrice(price: number) {
+    return new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price)
+  }
+
+  if (isLoading) {
+    return (
+      <Skeleton />
+    )
+  }
+
   return (
     <div className="w-full max-w-[1440px] px-12 mx-auto my-8">
       <div className="w-full grid grid-cols-3 gap-10">
-        {Array.from([1, 2, 3, 4, 5, 6, 7]).map((product) => (
-          <div className="border border-neutral-200 rounded-lg p-8" key={`product-${product}`}>
-            <div className="w-full h-48 mx-auto">
-              {/* Image */}
+        {data?.products.map((product) => (
+          <div className="border border-neutral-200 rounded-lg p-8" key={product.id}>
+            <div className="w-80 h-48 mx-auto overflow-hidden flex justify-center">
+              <img src={product.images[0]} alt={product.title} className="max-w-full max-h-full" />
             </div>
 
-            <h3 className="text-center text-lg text-neutral-900 font-bold">Nome produto</h3>
-            <p className="text-center text-sm text-neutral-900 mt-3">Descrição do produto</p>
+            <h3 className="text-center text-lg text-neutral-900 font-bold">{product.title}</h3>
+            <p className="text-center text-sm text-neutral-900 mt-3">{product.description}</p>
 
             <div className="flex items-center justify-between mt-8">
               <span className="text-xl text-neutral-900 font-bold">
-                {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(99.90)}
+                {formatPrice(product.price)}
               </span>
 
               <button
