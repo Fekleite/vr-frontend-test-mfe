eecc87d4330fa7915908c44024b3a11c1cebecc5
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-17 23:09:27 -0300

    chore(vr-footer): add federation config on remote footer app

--- vr-footer/vite.config.ts
+++ vr-footer/vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ModuleFederationPlugin from "@originjs/vite-plugin-federation";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+		target: 'chrome89',
+    minify: false,
+    cssCodeSplit: false,
+	},
+  plugins: [
+    ModuleFederationPlugin({
+      name: "vr-footer",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Footer": "./src/components/Footer"
+      },
+      shared: ["react", "react-dom", "tailwindcss"],
+    }),
+    react()
+  ],
 })
