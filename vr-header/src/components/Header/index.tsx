b21cc611c109b8afce198dcccb6e5140ae84c2fe
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-18 00:54:24 -0300

    feat(vr-header): create modal component

--- vr-header/src/components/Header/index.tsx
+++ vr-header/src/components/Header/index.tsx
@@ -1,20 +1,40 @@
+import { useState } from 'react'
 import logo from '../../assets/logo-white.png'
 import { BagIcon } from '../BagIcon'
+import { Modal } from '../Modal'
 
 export default function Header() {
+  const [isCartOpen, setIsCartOpen] = useState(false)
+
+  function handleOpenCart() {
+    setIsCartOpen(true)
+  }
+
+  function handleCloseCart() {
+    setIsCartOpen(false)
+  }
+
   return (
     <header className='w-full bg-brand py-4'>
       <div className='w-full max-w-[1440px] px-12 mx-auto flex justify-between items-center'>
         <img src={logo} alt="VR" className='w-8 h-8' />
 
         <div className='flex'>
-          <button type='button' className='h-8 px-4 bg-neutral-900 rounded-full hover:bg-neutral-700 duration-200'>
+          <button
+            type='button'
+            className='h-8 px-4 bg-neutral-900 rounded-full hover:bg-neutral-700 duration-1000'
+            onClick={handleOpenCart}
+          >
             <BagIcon />
 
             <span className='sr-only'>Sacola</span>
           </button>
         </div>
       </div>
+
+      <Modal title='Compras' isOpen={isCartOpen} onClose={handleCloseCart} >
+        Testando
+      </Modal>
     </header>
   )
 }
---
+++ vr-header/src/components/Modal/index.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from "react"
+import { createPortal } from "react-dom"
+
+import { CloseIcon } from "../CloseIcon"
+
+interface IModalProps {
+  title: string
+  children: ReactNode
+  isOpen: boolean
+  onClose: () => void
+}
+
+export function Modal({ title, children, isOpen, onClose }: IModalProps){
+  return createPortal(
+    <>
+      <div className={`fixed inset-0 z-50 flex justify-end ${isOpen ? 'pointer-events-auto' : 'pointer-events-none'}`}>
+        <div className={`bg-neutral-900 opacity-50 absolute inset-0 ${isOpen ? 'block' : 'hidden'}`} />
+        <div
+          className={`w-full max-w-2xl h-screen flex flex-col px-10 py-4 bg-white relative transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
+        >
+          <header className="flex items-center justify-between py-6 border-b border-neutral-100">
+            <h3 className="text-2xl text-neutral-900 font-bold">{title}</h3>
+
+            <button type="button" className="p-2" onClick={onClose}>
+              <CloseIcon />
+            </button>
+          </header>
+
+          <div className="my-6 flex-1">
+            {children}
+          </div>
+
+          <footer className="flex items-center justify-between py-6 border-t border-neutral-100">
+            <button
+              type="button"
+              className="h-8 px-4 rounded-full bg-brand hover:opacity-80 duration-200 text-neutral-50 text-sm font-semibold"
+            >
+              Concluir compras
+            </button>
+
+            <button
+              type="button"
+              className="h-8 px-4 rounded-full hover:bg-neutral-50 duration-200 text-neutral-900 text-sm font-semibold"
+            >
+              Cancelar
+            </button>
+          </footer>
+        </div>
+      </div>
+    </>,
+    document.getElementById('modal-root')!
+  )
+}
