d1d0393183bb6ad395a5b69ab86ed1bb65a5531e
Fernanda Leite <dev.fernandaleite@gmail.com>
2024-10-17 23:14:59 -0300

    feat(vr-commerce): use remote header and footer on app host

--- vr-commerce/src/App.tsx
+++ vr-commerce/src/App.tsx
@@ -1,7 +1,16 @@
+import { lazy } from "react";
+
+// @ts-ignore
+const Header = lazy(async () => import("vr-header/Header"));
+// @ts-ignore
+const Footer = lazy(async () => import("vr-footer/Footer"));
+
 export function App() {
   return (
-    <div>
-      <span>VR Commerce</span>
-    </div>
+    <>
+      <Header />
+
+      <Footer />
+    </>
   )
 }
